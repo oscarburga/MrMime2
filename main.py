7ecd25a7309a58edcd978abeeba843c50a8a0027
oscarburga <51001981+oscarburga@users.noreply.github.com>
2022-06-02 10:41:39 -0500

    preparing for on-site nao test

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
-from shutil import move
-import numpy as np
-import sys
-import motion
-import argparse
-import qi
-import time
-
 from naoqi import ALProxy
 
-Motion = ALProxy("ALMotion","127.0.0.1",9559)
-Spech = ALProxy("ALTextToSpeech","127.0.0.1",9559)
+NAO_HOST = "127.0.0.1"
+NAO_PORT = 9559
+
+Motion = ALProxy("ALMotion", NAO_HOST, NAO_PORT)
+Speech = ALProxy("ALTextToSpeech", NAO_HOST, NAO_PORT)
+
 names = ['LElbowRoll', 'LShoulderRoll', 'LShoulderPitch']
 times = [[1.0], [1.2], [1.3]]
 
 Motion.wakeUp()
 Motion.moveInit()
 Motion.post.moveTo(0.5, 0, 0)
-Spech.say("Hola a todos!!")
+Speech.say("Hola a todos!!")
 for i in range(3):
     Motion.angleInterpolation(names, [-3.0, 1.2, -0.7], times, True)
     Motion.angleInterpolation(names, [-1.0, 1.2, -0.7], times, True)
